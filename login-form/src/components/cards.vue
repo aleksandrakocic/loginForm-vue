d9b6a0abdf0239921816c5f6dad2643927eb1a85
aleksandrakocic <32391316+aleksandrakocic@users.noreply.github.com>
2018-06-17 00:43:31 +0200

    cards

--- login-form/src/components/cards.vue
+++ login-form/src/components/cards.vue
@@ -13,27 +13,29 @@ I would expect each card to have a title, description, image and some other deta
  In addition, create a "Lights On/Off" button which enables or disables dark mode on the application. 
 Of course, the button label should change appropriately.
  -->
-<div id='wraper'>
-    <p>{{info}}</p>
-  <div id='nav'>
-    <ul>
-       <li> <a href=""></a> home </li>
-       <li> <a href=""></a> about </li>
-       <li> <a href=""></a> gallery</li>
-       <li> <a href=""></a>contact </li>
+<div id='wrap'>
+
+   <div id='nav'>
+     <ul>
+       <li> <a href=""></a> HOME </li>
+       <li> <a href=""></a> ABOUT </li>
+       <li> <a href=""></a> CONTACT </li>
     </ul>
   </div>
 
-  <div id='cards'>
 
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-
+  <div id='cards'
+      v-bind:key="one.name" 
+      v-for="one in info">
+      <ul>
+        <li><h1>{{one.name}} </h1></li>
+        <li><img v-bind:src="one.img" alt=""></li>
+        <li>price:  {{one.price}} $</li>
+        <li><p id='desc'>{{one.description}}</p></li>
+      </ul>
   </div>
 </div>
+
 
 
 
@@ -61,12 +63,79 @@ export default {
 
     mounted () {
       this.axios
-        .get('/https://api.myjson.com/bins/17skk6')
+        .get('https://api.myjson.com/bins/17skk6')
         .then(response => (this.info =response.data))
   }
 };
 
 
-
-
 </script>
+
+
+<style>
+
+#wrap{
+  background: black;
+  color: white;
+}
+
+#nav ul {
+  display: flex;
+  justify-content: center;
+  background: lightgray;
+  color: black;
+  /* padding-top: 10px;
+  padding-bottom: 10px; */
+  padding: 0px 10px;
+}
+
+
+#nav ul li {
+/* margin-left: 5%; */
+padding: 35px;
+float: left;
+list-style-type: none;
+}
+#nav ul li a {
+  text-decoration: none;
+}
+
+
+
+#cards{
+  clear: both;
+  display: flex;
+  justify-content: center;
+}
+
+ #cards ul {
+  list-style: none;
+}
+ #cards ul li {
+  width: 600px;
+  text-align:center;
+  margin-top: 40px;
+}
+
+ #desc{
+  background: gray;
+  color: black;
+  padding: 15px;;
+}
+
+#cards ul li h1{
+  font-size: 20px;
+  font-weight: 800;
+  text-transform: uppercase;
+}
+
+#cards ul li img {
+  height: 300px;
+  width: 300px;
+}
+
+
+</style>
+
+
+
